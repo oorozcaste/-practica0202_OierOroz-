2c2909cf197a25f31423c89acb0e0273e284079f
oorozcaste <145026396+oorozcaste@users.noreply.github.com>
2023-10-23 19:50:31 +0200

    2

---
+++ ejercicio02.py
@@ -0,0 +1,10 @@
+numero1 = float(input("introduzca un numero: "))
+numero2 = float(input("introduzca un segundo numero: "))
+
+
+
+if numero2 == 0 :
+     print("error")
+else:
+     resultado = numero1/numero2
+     print("el resultado es",resultado)
